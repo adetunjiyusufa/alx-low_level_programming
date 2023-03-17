d4c48ea1dcfdff820c687c68947edd73821cd6fe
ADETUNJI ADEDAMOLA <ben2damola@gmail.com>
2023-03-17 14:49:42 -0700

    C-variables

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -10,7 +10,14 @@ int main(void)
 	char letter;
 
 	for (letter = 'a'; letter <= 'z'; letter++)
+	{
 			putchar(letter);
+	}
+
+	for (letter = 'A'; letter <= 'Z'; letter++)
+	{
+ 		putchar(letter);
+	}
 
 		putchar('\n');
 
