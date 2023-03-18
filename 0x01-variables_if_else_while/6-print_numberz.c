c416c1623aae6e11e215a2cec831cd2726e38ebc
ADETUNJI ADEDAMOLA <ben2damola@gmail.com>
2023-03-17 23:44:27 -0700

    C-variables

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+ * main -print all single digit numbers of base 10 starting from 0 with putchar
+ *
+ * Return: 0
+ *
+*/
+int main(void)
+{
+int n;
+
+	for (n = 0; n < 10; n++)
+
+	{
+		putchar("%d", n);
+	}
+
+	putchar("\n");
+	return (0);
+}
