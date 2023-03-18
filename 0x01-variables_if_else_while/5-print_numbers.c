9c3c320d37b7b70447455103a85721a7ef44ecc3
ADETUNJI ADEDAMOLA <ben2damola@gmail.com>
2023-03-17 23:20:49 -0700

    C-variables

--- 0x01-variables_if_else_while/5-print_numbers.c
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -1,8 +1,8 @@
 #include <stdio.h>
 /**
- *  main - to print all single digit numbers of base 10 starting from 0
- *
- *  Return : 0
+ * main - to print all single digit numbers of base 10 starting from 0
+ * and print the last digit of the number stored in the variable n
+ * Return: 0
  *
 */
 int main(void)
@@ -12,7 +12,7 @@ int main(void)
 	for (n = 0; n < 10; n++)
 
 	{
-		printf("%d"; n);
+		printf("%d", n);
 	}
 
 	printf("\n");
