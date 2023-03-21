42493ea4bd779acdcfa11e5ced575d6132e653cd
ADETUNJI ADEDAMOLA <ben2damola@gmail.com>
2023-03-21 13:44:14 -0700

    C-functions nested loops

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -4,7 +4,6 @@
  * print_alphabet_x10 - to  prints 10 times the alphabet, in lowercase,
  * followed by a new line.
  *
- * Return: 0
 */
 void print_alphabet_x10(void)
 {
@@ -18,5 +17,4 @@ void print_alphabet_x10(void)
 
 		_putchar('\n');
 	}
-	return (0);
 }
