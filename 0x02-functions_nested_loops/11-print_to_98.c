184b089a73ec51a637b29e2aec704bb68812d7d1
ADETUNJI ADEDAMOLA <ben2damola@gmail.com>
2023-03-21 16:08:40 -0700

    C-functions nested loops

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,31 @@
+#include "main.h"
+
+/**
+ * void print - to prints all natural numbers from n to 98,
+ *	followed by a new line.
+ * Numbers must be separated by a comma,
+ * followed by a space Numbers should be printed in order
+ * The first printed number should be the number passed to your function
+ * The last printed number should be 98
+ * @n: is the number to be imput
+ *
+ *
+*/
+
+void print_to_98(int n);
+{
+	if (n >= 98)
+	{
+		while (n > 98)
+			printf("%d, ", n--);
+		printf("%d\n", n);
+	}
+	else
+	{
+		while (n < 98)
+			printf("%d, ", n++);
+		printf("%d\n", n);
+	}
+}
+
+
