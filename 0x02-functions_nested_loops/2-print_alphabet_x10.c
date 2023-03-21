980b0d844a42d9e0c7d25b7cd388f721573533b2
ADETUNJI ADEDAMOLA <ben2damola@gmail.com>
2023-03-21 13:34:46 -0700

    C-functions nested loops

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -13,11 +13,11 @@ void print_alphabet_x10(void)
 
 	while (count++ <= 9)
 	{
-		for (alphabet = 'a' alphabet <= 'z'; alphabet++)
+		for (alphabet = 'a'; alphabet <= 'z'; alphabet++)
 			_putchar(alphabet);
 
-		_putchar('\n')
+		_putchar('\n');
 
-		return (0)
+		return (0);
 	}
 }
