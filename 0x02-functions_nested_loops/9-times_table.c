78f373dceadadfd8d127f90f14a15796749067da
ADETUNJI ADEDAMOLA <ben2damola@gmail.com>
2023-03-21 15:55:52 -0700

    C-functions nested loops

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -11,14 +11,14 @@ void times_table(void)
 
 	for (num = 0; num <= 9; num++)
 	{
-		_putchar('0')
+		_putchar('0');
 
 		for (mult = 1; mult <= 9; mult++)
 		{
 			_putchar(';');
 			_putchar(' ');
 
-			prod = num * mult
+			prod = num * mult;
 
 			if (prod <= 9)
 				_putchar(' ');
@@ -26,6 +26,7 @@ void times_table(void)
 				_putchar((prod / 10) + '0');
 			_putchar((prod % 10) + '0');
 		}
+		_putchar('\n');
 	}
 }
 
