e4394ec99e9e70b6a551613eac9e0f01d6ae8193
ADETUNJI ADEDAMOLA <ben2damola@gmail.com>
2023-03-21 15:51:18 -0700

    C-functions nested loops

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * add - to adds two integers and returns the result.
+ * @m: first integer
+ * @n: second integer
+ * Return: the addition of m and n
+ *
+ *
+*/
+
+int add(int m, int n)
+{
+	return (m + n);
+}
+
