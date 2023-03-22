e5de8eead308d372e6fb2f056fc64c7894929864
ADETUNJI ADEDAMOLA <ben2damola@gmail.com>
2023-03-22 13:56:16 -0700

    C-functions nested loops

---
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+
+/**
+ * main - prints the first 50 Fibonacci numbers, starting with 1 and 2,
+ *	followed by a new line
+ *	The numbers must be separated by comma, followed by a space ,
+ * Return: 0
+ *
+*/
+
+int main(void)
+{
+	int count;
+	unsigned long fnum1 = 1, fnum2 = 2, sum;
+
+	for (count = 0; count < 50; count++)
+	{
+		sum = fnum1 + fnum2;
+		putchar('sum');
+
+			fnum1 = fnum2;
+			fnum2 = sum;
+
+			if (count == 49)
+				putchar('\n');
+			else
+				putchar(', ');
+	}
+	return (0);
+}
