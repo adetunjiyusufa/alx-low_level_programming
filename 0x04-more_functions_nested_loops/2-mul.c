e754356c46f77efdb62d573c8fa80ca69e6fbbd6
ADETUNJI ADEDAMOLA <ben2damola@gmail.com>
2023-03-23 12:52:45 -0700

    more nested loops

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * mul - to multiplies two integers.
+ * @a: first number to be multiply
+ * @b: second number to be multiply
+ *
+ */
+
+int mul(int a, int b)
+{
+	int a, b, prod;
+
+	prod = a * b
+		printf("%d" prod)
+}
