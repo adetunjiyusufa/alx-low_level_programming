1fea1ad66eb22144c1033e9cc7a1282a46503b01
ADETUNJI ADEDAMOLA <ben2damola@gmail.com>
2023-03-23 15:00:16 -0700

    more nested loops

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * more_numbers - to print more numbers
+ *
+ */
+
+void more_numbers(void)
+{
+	int m, n;
+
+	for (m = 1; m <= 10; m++)
+	{
+		for (n = 0; n <= 14; n++)
+		{
+			if (n >= 10)
+				_putchar('1');
+			_putchar(n % 10 + '0');
+		}
+		_putchar('\n');
+	}
+}
