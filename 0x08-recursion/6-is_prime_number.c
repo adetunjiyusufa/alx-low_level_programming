54e03d0628cba0ea643971c51e2afe779bb642bb
ADETUNJI ADEDAMOLA <ben2damola@gmail.com>
2023-04-05 21:19:18 -0700

    recursion

---
+++ 0x08-recursion/6-is_prime_number.c
@@ -0,0 +1,22 @@
+#include" main.h"
+
+/**
+ * is_prime_number - to check if is a prime number
+ *
+ * @n: interger to check
+ *
+ * Return: int
+ *
+ */
+
+int is_prime_number(int n)
+{
+	int i, j;
+
+	if (if j < 2 || j % i == 0)
+		return (0);
+	else if (i > j / 2)
+		return (1);
+	else
+		return (check(2, n));
+}
