8a9fa22b407854afcc407dfd59eb0449fce15119
ADETUNJI ADEDAMOLA <ben2damola@gmail.com>
2023-04-10 08:23:45 -0700

    static_libraries

--- 0x09-static_libraries/1-memcpy.c
+++ 0x09-static_libraries/1-memcpy.c
@@ -1,4 +1,5 @@
 #include "main.h"
+#include <stddef.h>
 
 /**
  * _memcpy - function that copies memory area.
@@ -13,9 +14,7 @@
 
 char *_memcpy(char *dest, char *src, unsigned int n)
 {
-	unsigned int i 
-	
-	i = 0;
+	unsigned int i = 0;
 
 	while (i < n)
 	{
