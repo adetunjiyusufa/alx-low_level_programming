43050f0056d082f18c6fcbb40e3b8d41c49890eb
ADETUNJI ADEDAMOLA <ben2damola@gmail.com>
2023-04-13 14:44:48 -0700

    malloc

---
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * malloc_checked - to allocates memory using malloc
+ *
+ * @b: the memory allocated
+ *
+ * Return: a pointer to the allocated memory
+ *
+ */
+
+void *malloc_checked(unsigned int b)
+{
+	void *x;
+
+	x = malloc(b);
+	if (i == NULL)
+		exit(98);
+	return (x);
+}
---
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -0,0 +1,31 @@
+#include "main.h"
+
+/**
+ * _calloc - to allocates memory for an array, using malloc.
+ *
+ * @size: the allocate element of size bytes
+ * @nmemb: allocate memory for an array
+ *
+ * Return: the pointer to the allocated memory
+ *
+ */
+
+void *_calloc(unsigned int nmemb, unsigned int size)
+{
+	char *x;
+	unsigned int y;
+
+	if (nmemb == 0 || size == 0)
+		return (NULL);
+
+	x = malloc(nmemb * size);
+	if (x == NULL)
+	return (NULL);
+
+	for (y = 0; y < (nmemb * size); y++)
+		a[b] = 0;
+
+	return (x);
+}
+
+
