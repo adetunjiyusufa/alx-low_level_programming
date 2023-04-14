b719be8c351240a05a27f42c3fade8932b11572b
ADETUNJI ADEDAMOLA <ben2damola@gmail.com>
2023-04-13 21:48:09 -0700

    malloc

--- 0x0C-more_malloc_free/main.h
+++ 0x0C-more_malloc_free/main.h
@@ -6,6 +6,8 @@ void *malloc_checked(unsigned int b);
 char *string_nconcat(char *s1, char *s2, unsigned int n);
 void *_calloc(unsigned int nmemb, unsigned int size);
 int *array_range(int min, int max);
-
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 
 #endif
