56996c2d962c71695e5510f09be70bf98d83c1e3
ADETUNJI ADEDAMOLA <ben2damola@gmail.com>
2023-04-21 13:12:27 -0700

    variadic_functions

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -30,7 +30,7 @@ void print_all(const char * const format, ...)
 				flag = 0;
 				break;
 			case 'f':
-				printf("%f", va_arg(alist, double));
+				printf("%f", va_arg(a_list, double));
 				flag = 0;
 				break;
 			case 's':
